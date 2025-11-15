6e5d9588a5eb7ac8a450fe0077057c64d4c5dbe0
ak <134972308+Ay-Kay8@users.noreply.github.com>
2025-11-15 01:32:24 -0500

    Implemented User class

---
+++ bank/bank/User.java
@@ -0,0 +1,59 @@
+package bank;
+import java.util.Date;
+
+public abstract class User {
+    private long id;
+    private String username;
+    private String password;
+    private String emailString;
+    private Role role;
+    private Date createdDate;
+
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailString() {
+        return emailString;
+    }
+
+    public void setEmailString(String emailString) {
+        this.emailString = emailString;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
