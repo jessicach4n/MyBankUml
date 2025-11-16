350ae75de012b095d090cd1ff2650f890cf2ac27
ak <134972308+Ay-Kay8@users.noreply.github.com>
2025-11-15 20:41:09 -0500

    Implemented Employee class

---
+++ bank/user/Employee.java
@@ -0,0 +1,26 @@
+package user;
+import branch.Branch;
+
+public class Employee extends User{
+    private long employeeId;
+    private Branch branch;
+
+    public Employee(String username, String password, String emailString, Role role, long employeeId, Branch branch) {
+        super(username, password, emailString, role);
+        super.setId(employeeId);
+        this.branch = branch;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+    public Branch getBranch() {
+        return branch;
+    }
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+}
