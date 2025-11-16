efc1d55c8828b7ed5b8a44808cc2f7c95d5887cd
ak <134972308+Ay-Kay8@users.noreply.github.com>
2025-11-15 21:07:34 -0500

    Implemented Account

--- bank/account/Account.java
+++ bank/account/Account.java
@@ -6,7 +6,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Account {
-    protected Customer customer;
+    
+    static long idCounter = 0;
+    private long id;
+    private String accountNumber;
+    private String accountType;
+    private double balance;
+    private String status;
+    protected Customer customer; // Should this be here?
+    
+    public Account(String accountNumber, String accountType, double balance, String status, Customer customer,
+    List<Transaction> transactions) {
+        this.id = generateId();
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.status = status;
+        this.customer = customer;
+        this.transactions = transactions;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    private long generateId() {
+        return ++idCounter;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
     protected List<Transaction> transactions;
 
     public Account(Customer customer) {
