d23f1c9cbdef9345210dd2ae904607ec5bb120fd
ak <134972308+Ay-Kay8@users.noreply.github.com>
2025-11-24 23:40:50 -0500

    bank bank bank bwanch mannger impelemntation

--- src/main/java/bank/branch/Bank.java
+++ src/main/java/bank/branch/Bank.java
@@ -1,30 +1,52 @@
 package bank.branch;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
+    private static int idCounter = 0;
+    private final int bankID;
     private final String name;
     private final List<Branch> branches;
 
     public Bank(String name) {
+        this.bankID = generateID();
         this.name = name;
         this.branches = new ArrayList<>();
     }
 
+    private int generateID(){
+        return ++idCounter;
+    }
+
+    // Method used by BranchManagement.addBranch
     public void addBranch(Branch branch) {
-        branches.add(branch);
+        if (!branches.contains(branch)) {
+            branches.add(branch);
+        } else {
+            System.out.println("Branch " + branch.getBranchName() + " (id: " + branch.getBranchID() + ") is already in the bank.");
+        }
+    }
+
+    // Method used by BranchManagement.removeBranch
+    // Returns true if it was successfully removed
+    public boolean removeBranch(Branch branch) {
+        return branches.remove(branch);
+    }
+    
+    public Branch getBranch(int index){
+        return branches.get(index);
+    }
+
+    public int getBankID() {
+        return bankID;
     }
 
     public String getName() {
         return name;
     }
-
-    public void printBankInfo() {
-        System.out.println("Bank: " + name);
-        for (Branch branch : branches) {
-            System.out.println("Branch: " + branch.getAddress());
-        }
+    
+    public List<Branch> getBranches() {
+        return branches;
     }
 }
---
+++ src/main/java/bank/branch/BranchManagement.java
@@ -0,0 +1,31 @@
+package bank.branch;
+
+// This implementation is super not robust, but I think its ok
+public class BranchManagement {
+    
+    public BranchManagement() {
+
+    }
+
+    // Return the newly created Branch object
+    public Branch addBranch(String branchName, String address, Bank bank) {
+        Branch newBranch = new Branch(branchName, address, bank);
+        bank.addBranch(newBranch);
+        System.out.println("Branch added successfully: " + branchName);
+        return newBranch;
+    }
+
+    // Returns true if the branch was found and removed, false otherwise.
+    public boolean removeBranch(Branch branch) {
+        return branch.getBank().removeBranch(branch); 
+    }
+
+    public Branch getBranchInfo(Branch branch) {
+        return branch;
+    }
+
+    public void updateBranch(Branch branch, String newName, String newAddress) {
+        branch.setAddress(newAddress);
+        branch.setBranchName(newName);
+    }
+}
